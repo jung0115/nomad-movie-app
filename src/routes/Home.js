da692ecadb4d1382f105170ce630bc3bc4075998
jung0115 <jungim7490@gmail.com>
2022-11-04 19:19:37 +0900

    styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -24,7 +24,9 @@ function Home() {
   return (
     <div className={styles.container}>
       {loading ? (
-        <h1>Loading...</h1>
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
       ) : (
         <div className={styles.movies}>
           {movies.map(movie => (
