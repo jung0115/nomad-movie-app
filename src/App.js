326d9a91ffc9a9061d90000a08fd19ad9ed8a453
jung0115 <jungim7490@gmail.com>
2022-11-04 17:58:10 +0900

    save

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import Detail from "./routes/Detail";
 function App() {
   return <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail />} />
-      <Route path="/" element={<Home />} />
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
     </Routes>
   </Router>;
 }
